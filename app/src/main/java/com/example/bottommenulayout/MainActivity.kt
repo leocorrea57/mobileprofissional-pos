e30016456cf33fc77f7ed6dadd294347e348ff54
Leonardo Souza <leocorrea@edu.unifil.br>
2023-10-10 23:16:39 -0300

    commit teste

--- app/src/main/java/com/example/bottommenulayout/MainActivity.kt
+++ app/src/main/java/com/example/bottommenulayout/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bottommenulayout
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+
+        navView.isVisible = true
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
