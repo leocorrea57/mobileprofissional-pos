7fbf17c59a1dee20bf22cb8a4e7bcafb10093212
Leonardo Souza <leocorrea@edu.unifil.br>
2023-10-13 21:41:03 -0300

    Atualização do resgistro de item de cardápio

--- app/src/main/java/com/example/bottommenulayout/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/bottommenulayout/ui/register/RegisterFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.bottommenulayout.R
 import com.example.bottommenulayout.databinding.FragmentRegisterBinding
+import com.example.bottommenulayout.model.DataStore
+import com.example.bottommenulayout.model.FoodMenuItem
 import com.example.bottommenulayout.model.Restaurante
+import com.example.bottommenulayout.ui.foodmenu.FoodMenuViewModel
+import java.math.BigDecimal
 
 class RegisterFragment : Fragment() {
 
@@ -15,45 +22,56 @@ class RegisterFragment : Fragment() {
     private val binding get() = _binding!!
 
     private var position = -1
-    private var restaurante = Restaurante()
+    private lateinit var foodMenuItem : FoodMenuItem
+    private lateinit var registerViewModel: RegisterViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
+
+
+        val registerViewModel =
             ViewModelProvider(this).get(RegisterViewModel::class.java)
 
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        binding.btnAdicionar.setOnClickListener {
-//
-//            restaurante.nome = binding.fieldNome.text.toString()
-//            restaurante.tipo = binding.fieldTipo.text.toString()
-//            restaurante.taxa = binding.fieldTaxa.text.toString().toBigDecimal()
-//
-//            val preferences =
-//                context?.getSharedPreferences(getString(R.string.file_preferences), Context.MODE_PRIVATE)
-//
-//            DataStore.addRestaurante(restaurante)
-//
-//            preferences?.edit()?.apply() {
-//                val gson = Gson()
-//                val json = gson.toJson(
-//                    DataStore.restaurantes
-//                )
-//                putString(getString(R.string.rest), json)
-//                commit()
-//            }
-//
-//            Toast.makeText(
-//                activity,
-//                "Restaurante ${binding.fieldNome.text.toString()} adicionado!",
-//                Toast.LENGTH_LONG
-//            ).show()
-//        }
+        val spinner = binding.categorySpinner
+
+        this.context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.itens_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears.
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner.
+                spinner.adapter = adapter
+            }
+        }
+
+        binding.btnAdicionar.setOnClickListener {
+
+            //val value = binding.fieldValor.get
+
+            foodMenuItem = FoodMenuItem(
+                nome = binding.fieldNome.text.toString(),
+                categoria = binding.categorySpinner.selectedItem.toString(),
+                descricao = binding.fieldDesc.text.toString(),
+                valor = BigDecimal.TEN // Deu errado -> binding.fieldValor.text.toString().toBigDecimal()
+            )
+
+            registerViewModel.addFoodMenuItem(foodMenuItem)
+
+            Toast.makeText(
+                activity,
+                "Item ${binding.fieldNome.text.toString()} adicionado!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
 
         return root
     }
--- app/src/main/java/com/example/bottommenulayout/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/bottommenulayout/ui/register/RegisterViewModel.kt
@@ -3,6 +3,9 @@ package com.example.bottommenulayout.ui.register
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.bottommenulayout.model.CategoriasPizzaria
+import com.example.bottommenulayout.model.DataStore
+import com.example.bottommenulayout.model.FoodMenuItem
 
 class RegisterViewModel : ViewModel() {
 
@@ -10,4 +13,9 @@ class RegisterViewModel : ViewModel() {
         value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
+
+
+    fun addFoodMenuItem(item : FoodMenuItem) {
+        DataStore.cardapio.add(item)
+    }
 }
